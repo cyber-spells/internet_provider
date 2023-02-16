04f7a962ebcbffcf979ff64ed932fdf507e56379
kolan4ick <Kolan4ick@gmail.com>
2023-02-16 18:17:17 +0200

    Change timezone to Europe/Kyiv

--- config/application.rb
+++ config/application.rb
@@ -24,6 +24,9 @@ module InternetProvider
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
 
+    config.time_zone = 'Europe/Kyiv'
+    config.active_record.default_timezone = :local
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
